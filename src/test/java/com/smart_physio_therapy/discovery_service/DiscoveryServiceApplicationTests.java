3c65d716d033022462b447ae13b1477a0f1af2c4
Tuan Kiet <limit389@gmail.com>
2025-05-27 01:47:47 +0700

    Init: setup-eureka Summary: - Setup Eureka as Discovery Service

---
+++ src/test/java/com/smart_physio_therapy/discovery_service/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_physio_therapy.discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
